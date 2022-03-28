d07d1d35461a6083b69113f99e7de7723ba16eea
Hennell <git@hennell.dev>
2022-03-28 15:38:34 +0100

    Basic page setup to test translating

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+               <h1 class="text-6xl font-thin">Hello!</h1>
+                <p class="text-gray-800 text-xl">This is some dummy text to translate</p>
+            </div>
+        </div>
+    </body>
+</html>
